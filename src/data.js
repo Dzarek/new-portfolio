82de6b5931942b1e94b8c8742f139af2417de331
Jarek <jarekjanas95@gmail.com>
2021-11-17 00:02:12 +0100

    projects data

--- src/data.js
+++ src/data.js
@@ -2,6 +2,13 @@ import dorotaSzydelkujeImg from "./images/dorotaSzydelkujeImg.jpg";
 import robertTelefonyImg from "./images/robertTelefonyImg.jpg";
 import jjApartmentsImg from "./images/jjApartmentsImg.jpg";
 import jaroslawGotujeImg from "./images/jaroslawGotujeImg.jpg";
+import buyListImg from "./images/buyListImg.jpg";
+import cocktailsChillImg from "./images/cocktailsChillImg.jpg";
+import wheatherImg from "./images/wheatherImg.jpg";
+import serialQuizImg from "./images/serialQuizImg.jpg";
+import ninjaVsZombieImg from "./images/ninjaVsZomnieImg.jpg";
+import wisielecImg from "./images/wisielecImg.jpg";
+import memoryImg from "./images/memoryImg.jpg";
 
 export const projects = [
   {
@@ -9,7 +16,7 @@ export const projects = [
     title: "Dorota Szydełkuje",
     img: dorotaSzydelkujeImg,
     info: "Strona internetowa stworzona dla osoby, która zajmuje się szydełkowaniem. Można tam zamówić produkty wykonane z włóczki takie jak: zabawki, pufy, koce itd.",
-    technology: ["React", "Css"],
+    technology: ["React"],
     githubLink: "https://github.com/Dzarek/dorotaszydelkuje2",
     siteLink: "https://www.dorotaszydelkuje.com",
     category: "website",
@@ -19,7 +26,7 @@ export const projects = [
     title: "Robert Kamiński - Telefony Komórkowe",
     img: robertTelefonyImg,
     info: "Strona internetowa salonu GSM, w którym można kupić, sprzedać lub oddać do serwisu swój smartfon.",
-    technology: ["React", "Css"],
+    technology: ["React"],
     githubLink: "https://github.com/Dzarek/RobertTelefony",
     siteLink: "https://robert-telefony.netlify.app/",
     category: "website",
@@ -29,7 +36,7 @@ export const projects = [
     title: "JJ Apartments",
     img: jjApartmentsImg,
     info: "Strona internetowa hotelu/apartamentów do wynajęcia. Umożliwia sprawdzenie pokoi i ich filtrowanie,oglądnięcie galerii zdjęć oraz zarezerwowanie pokoju poprzez przekierowanie do booking.com",
-    technology: ["React", "Css"],
+    technology: ["React"],
     githubLink: "https://github.com/Dzarek/jjapartments",
     siteLink: "https://jjapartments.netlify.app/",
     category: "website",
@@ -39,9 +46,79 @@ export const projects = [
     title: "Jarosław Gotuje",
     img: jaroslawGotujeImg,
     info: "Strona internetowa z przepisami kulinarnymi mistrza kuchni - Jarosława",
-    technology: ["Gatsby", "Css"],
+    technology: ["Gatsby"],
     githubLink: "https://github.com/Dzarek/jaroslaw-gotuje",
     siteLink: "https://jaroslaw-gotuje.netlify.app/",
     category: "website",
+  },
+  {
+    id: 5,
+    title: "Lista Zakupów",
+    img: buyListImg,
+    info: "Aplikacja do zapisywania swojej listy zakupów. Bardzo pomaga w organizacji zakupów.",
+    technology: ["JavaScript"],
+    githubLink: "https://github.com/Dzarek/buyList",
+    siteLink: "https://buylist.netlify.app/",
+    category: "app",
+  },
+  {
+    id: 6,
+    title: "Cocktails-Chill",
+    img: cocktailsChillImg,
+    info: "Aplikacja do szukania przepisów na swoje ulubione drinki. Niezwykle przydatna na imprezach.",
+    technology: ["React"],
+    githubLink: "https://github.com/Dzarek/CocktailsChill",
+    siteLink: "https://cocktails-chill.netlify.app/",
+    category: "app",
+  },
+  {
+    id: 7,
+    title: "Aplikacja Pogodowa",
+    img: wheatherImg,
+    info: "Aplikacja do sprawdzania pogody prawie dla każdego miasta na świecie.",
+    technology: ["React"],
+    githubLink: "https://github.com/Dzarek/pogoda",
+    siteLink: "https://dzarek.github.io/pogoda/",
+    category: "app",
+  },
+  {
+    id: 8,
+    title: "Serialowe Quizowanie",
+    img: serialQuizImg,
+    info: "Coś dla fanów oglądania seriali! Pochwal się swoją wiedzą z różnych serialowych hitów i zdobądź jak najwięcej punktów.",
+    technology: ["JavaScript"],
+    githubLink: "https://github.com/Dzarek/serialoweQuizowanie",
+    siteLink: "https://dzarek.github.io/serialoweQuizowanie/",
+    category: "game",
+  },
+  {
+    id: 9,
+    title: "Ninja Vs Zombie",
+    img: ninjaVsZombieImg,
+    info: "Pozabijaj wszystkie zombiaki bo inaczej Cię zjedzą...",
+    technology: ["JavaScript"],
+    githubLink: "https://github.com/Dzarek/ninjaGame",
+    siteLink: "https://dzarek.github.io/ninjaGame/",
+    category: "game",
+  },
+  {
+    id: 10,
+    title: "Wisielec",
+    img: wisielecImg,
+    info: "Pamiętacie czasy gdy na nudnych lekcjach w szkole wraz z kolegami graliście w różne gry na kartce papieru? Jeśli macie ochotę zabić nudę to proponuję wam moją wersję gry WISIELEC.",
+    technology: ["JavaScript"],
+    githubLink: "https://github.com/Dzarek/wisielecGame",
+    siteLink: "https://dzarek.github.io/wisielecGame/",
+    category: "game",
+  },
+  {
+    id: 11,
+    title: "Memory",
+    img: memoryImg,
+    info: "Czy dobrą masz pamięć? Sprawdź i zagraj w MEMORY uzyskując jak najlepszy czas!",
+    technology: ["JavaScript"],
+    githubLink: "https://github.com/Dzarek/memoryGame",
+    siteLink: "https://dzarek.github.io/memoryGame/",
+    category: "game",
   },
 ];
